3735d7b4637fd42ca16f19d1ef41180f8a594d82
Flexilis-anatis <Flexilisanatis@protonmail.com>
2019-01-28 20:45:52 -0700

    Started basic structure

--- line_counter.py
+++ line_counter.py
@@ -7,7 +7,7 @@ total = 0
 for root, dirs, files in os.walk("."):
 	path = root.split(os.sep)
 	for file in files:
-		if "hashtable" in root or "vector" in file or (".c" not in file and ".h" not in file):
+		if "line_counter" in file or (".py" not in file):
 			continue
 		loc = 0
 		with open(root+os.sep+file) as f:
